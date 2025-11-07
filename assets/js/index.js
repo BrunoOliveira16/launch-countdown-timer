0e3a19f8312e203b0119c5ee97cf2135cec6a000
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2025-11-07 19:50:13 -0300

    feat: stop countdown at zero upon completion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,28 @@
 const countDate = new Date("dez 31, 2026 00:00:00").getTime();
 
 let x = setInterval(()=> {
-
     const today = new Date().getTime();
-
     let distance = countDate - today;
 
-    let days = Math.floor(distance / (1000*60*60*24));
-    let hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
-    let minutes = Math.floor((distance % (1000*60*60))/(1000*60));
-    let seconds = Math.floor((distance % (1000*60))/(1000));
+    if (distance <= 0) {
+        // Para a execução do setInterval (impede que rode para sempre)
+        clearInterval(x);
 
-    document.getElementById('days').innerHTML = days
-    document.getElementById('hours').innerHTML = hours
-    document.getElementById('minutes').innerHTML = minutes
-    document.getElementById('seconds').innerHTML = seconds
+        // Trava a exibição em 0
+        document.getElementById('days').innerHTML = 0;
+        document.getElementById('hours').innerHTML = 0;
+        document.getElementById('minutes').innerHTML = 0;
+        document.getElementById('seconds').innerHTML = 0;
+    } else {
+        let days = Math.floor(distance / (1000*60*60*24));
+        let hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+        let minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+        let seconds = Math.floor((distance % (1000*60))/(1000));
+
+        document.getElementById('days').innerHTML = days
+        document.getElementById('hours').innerHTML = hours
+        document.getElementById('minutes').innerHTML = minutes
+        document.getElementById('seconds').innerHTML = seconds
+    }
 }, 1000);
 
